f9d4962bac05ecab5b2dc52b8b0d564af446b7a6
Sarim Abbas <sarim.stays@gmail.com>
2018-04-30 15:54:12 -0400

    simple

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-import os
-import sys
-import requests
+from flask import Flask
 
-# database models (later)
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import joinedload
-# from flask_migrate import Migrate
+app = Flask(__name__)
 
-# parse requests (looks inside query, form and JSON data)
-from datetime import date, datetime
-from webargs import validate, fields, ValidationError
-from webargs.flaskparser import parser, abort, use_args
-
-# Flask web framework
-from flask import Flask, g, request, jsonify, render_template
-
-# CONNECTIONS
-app = Flask(__name__)                   # Flask
-# app.config.from_object(Config)          # config
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return 'Hello world'
 
-if __name__ == "__main__":
-    app.run(debug=True, port=80, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
