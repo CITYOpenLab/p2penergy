ed830427cfe621d3efb908ac0134d1ec604ab319
Sarim Abbas <sarim.stays@gmail.com>
2018-04-30 16:42:37 -0400

    revert

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import os
+import sys
+import requests
 
-app = Flask(__name__)
+# database models (later)
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import joinedload
+# from flask_migrate import Migrate
 
-@app.route('/')
+# parse requests (looks inside query, form and JSON data)
+from datetime import date, datetime
+from webargs import validate, fields, ValidationError
+from webargs.flaskparser import parser, abort, use_args
+
+# Flask web framework
+from flask import Flask, g, request, jsonify, render_template
+
+# CONNECTIONS
+app = Flask(__name__)                   # Flask
+# app.config.from_object(Config)          # config
+
+
+@app.route("/")
 def index():
-    return 'Hello world'
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
