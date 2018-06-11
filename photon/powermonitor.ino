0e689104fedd2e159bfe93ac9eb5cc2c7a4720ca
Sarim Abbas <sarim.stays@gmail.com>
2018-06-12 01:13:47 +0500

    photon mqtt

--- photon/powermonitor.ino
+++ photon/powermonitor.ino
@@ -1,3 +1,4 @@
+#include <MQTT.h>
 #include "math.h"
 
 // RMS voltage
@@ -9,7 +10,48 @@ const int numTurns = 2000;      // 1:2000 transformer turns
 const int rBurden = 100;        // Burden resistor value
 const int numSamples = 1000;    // Number of samples before calculating RMS
 
-void setup() {
+void callback(char* topic, byte* payload, unsigned int length);
+MQTT client("broker.hivemq.com", 1883, callback);
+
+// recieve message
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
+    
+    if (!strcmp(p, "RED"))
+    {
+        RGB.color(255, 0, 0);
+    }
+    else if (!strcmp(p, "GREEN"))
+    {
+        RGB.color(0, 255, 0);
+    }
+    else if (!strcmp(p, "BLUE"))
+    {
+        RGB.color(0, 0, 255);
+    }
+    else
+    {
+        RGB.color(255, 255, 255);
+    }
+    delay(1000);
+}
+
+void setup() 
+{
+    RGB.control(true);
+    
+    // connect to the server(unique id by Time.now())
+    client.connect("photon_" + String(Time.now()));
+    
+    // publish/subscribe
+    if (client.isConnected()) 
+    {
+        client.publish("p2penergy/photon/events", "hello world");
+        client.subscribe("p2penergy/photon/input");
+    }
     
 }
 
@@ -42,9 +84,19 @@ void loop() {
     
     // Calculate apparent power and publish it
     apparentPower = vRMS * iRMS;
-    Particle.publish("RMS Current", String(iRMS));
-    Particle.publish("RMS Voltage", String(vRMS));
-    Particle.publish("Apparent Power", String(apparentPower));
-
+    Particle.publish("rms_current", String(iRMS));
+    Particle.publish("rms_voltage", String(vRMS));
+    Particle.publish("power", String(apparentPower));
+    
+    // publish via mqtt
+    client.publish("p2penergy/photon/events", String(iRMS));
+    client.publish("p2penergy/photon/events", String(vRMS));
+    client.publish("p2penergy/photon/events", String(apparentPower));
+    
     delay(4000);
+    
+    if (client.isConnected()) {
+        client.loop();
+    }
+    
 }
